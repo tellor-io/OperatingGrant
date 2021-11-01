a3cb3f8450b3b9489df63308398bf00c3937002b
Nicholas Fett <nicholasfett@Nicholass-MacBook-Pro.local>
2021-11-01 14:47:36 -0400

    compiling

--- contracts/OperatingGrant.sol
+++ contracts/OperatingGrant.sol
@@ -12,12 +12,11 @@ contract OparatingGrant {
     //Storage
     uint256 public lastReleaseTime;
     uint256 public maxAmount;
-    address public beneficiary = 0x39e419ba25196794b595b2a595ea8e527ddc9856;
-    address public constant tellorAddress = 0x88df592f8eb5d7bd38bfef7deb0fbc02cf3778a0;
+    address public beneficiary = 0x39E419bA25196794B595B2a595Ea8E527ddC9856;
+    address public constant tellorAddress = 0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0;
 
     //Events
-    event VestedDeposit (uint _amount);
-    event TokenWithdrawal (uint _amount);
+    event TokenWithdrawal (uint256 _amount);
 
 
     constructor () {
@@ -38,11 +37,11 @@ contract OparatingGrant {
      *
      */
     function withdrawTrb() external {
-        uint256 _availableBalance = tellor.balanceOf(address(this));
+        uint256 _availableBalance = ITellor(tellorAddress).balanceOf(address(this));
         if(_availableBalance > maxAmount){
             maxAmount = _availableBalance;
         }
-        uint256 _releasedAmount = maxAmount * (block.timestamp - lastReleaseTime)/(86400* 365 * 2);
+        uint256 _releasedAmount = maxAmount * (block.timestamp - lastReleaseTime)/(86400* 365 * 2); //2 year payout 
         if(_releasedAmount > _availableBalance){
             _releasedAmount = _availableBalance;
         }
